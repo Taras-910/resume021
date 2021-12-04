f596af06386bc1e16acaf7c56634df729268de90
taras <promverstat@ukr.net>
2021-12-04 12:34:54 +0200

    2 10  debug filter

--- src/main/java/ua/training/top/util/ResumeUtil.java
+++ src/main/java/ua/training/top/util/ResumeUtil.java
@@ -6,6 +6,7 @@ import ua.training.top.model.Vote;
 import ua.training.top.to.ResumeTo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -62,27 +63,44 @@ public class ResumeUtil {
     }
 
     public static List<Resume> getFilter(List<Resume> resumes, Freshen f) {
-        return f.getLanguage().equals("all") && f.getLevel().equals("all") && f.getWorkplace().equals("all") ? resumes : resumes.stream()
-                .filter(r -> (f.getLanguage().equals("all") ||
-                        isExactly(of(r.getSkills(), r.getTitle(), r.getWorkBefore(), r.getFreshen().getLanguage()), f.getLanguage()))
-                && (f.getLevel().equals("all") || isContains(of(r.getTitle(), r.getWorkBefore(), r.getFreshen().getLevel()), f.getLevel()))
-                && (f.getWorkplace().equals("all") || isMatch(getAria(f.getWorkplace()), getBuild(r.getAddress()).append(r.getWorkBefore()))))
+        return resumes.stream().
+                filter(r -> isSuit(r, f.getLanguage(), "language") && isSuit(r, f.getLevel(), "level") &&
+                        isSuit(r, f.getWorkplace(), "workplace"))
                 .collect(Collectors.toList());
     }
 
-    public static boolean isExactly(List<String> list, String word) {
-        return list.stream().anyMatch(s -> s.toLowerCase().matches(".*\\b" + word + "\\b.*"));
+
+    public static boolean isSuit(Resume r, String field, String fieldKind) {
+        String text = fieldKind.equals("language") ?
+                getBuild(r.getSkills()).append(r.getTitle()).append(r.getFreshen().getLanguage()).toString().toLowerCase():
+                fieldKind.equals("level") ?
+                        getBuild(r.getTitle()).append(r.getWorkBefore()).append(r.getFreshen().getLevel()).toString().toLowerCase() :
+                        getBuild(r.getAddress()).append(r.getWorkBefore()).toString().toLowerCase();
+        return switch (field.toLowerCase()) {
+            case "all" -> true;
+            case "java", "react", "ruby" -> text.matches(".*\\b" + field + "\\b.*");
+            case "trainee", "стажировка", "стажер", "internship", "интерн",
+                    "intern" -> getAria(field).stream()
+                    .anyMatch(a -> text.matches(".*\\b" + a + "\\b.*"));
+            default -> getAria(field).size() == 1 ? text.indexOf(field) > -1 : getAria(field).stream()
+                    .anyMatch(a -> !isMatch(getForeign(), field) ? text.indexOf(a) > -1 : text.indexOf(a) > -1
+                            && ukraineAria.stream().noneMatch(cityUA -> text.toLowerCase().indexOf(cityUA) > -1));
+        };
     }
 
-    public static boolean isContains(List<String> area, String word) {
-        return area.stream().anyMatch(a -> a.toLowerCase().indexOf(word) > -1);
-    }
 
-    private static List<String> getAria(String workplace) {
-        return switch (workplace) {
-            case "ukraine", "україна", "украина" -> ukraineAria;
-            case "київ", "киев", "kiev" -> kievAria;
-            case "foreign", "за_рубежем" -> citiesWorld;
+    private static List<String> getAria(String text) {
+        return switch (text) {
+            case "intern", "trainee", "интерн", "стажировка", "стажер" -> traineeAria;
+            case "junior" -> juniorAria;
+            case "middle" -> middleAria;
+            case "senior" -> seniorAria;
+            case "expert", "lead", "тимлид", "team lead" -> expertAria;
+            case "ukraine", "україна", "украина", "ua" -> ukraineAria;
+            case "russia", "россия", "росія", "ru" -> russiaAria;
+            case "київ", "киев", "kiev", "kyiv" -> kievAria;
+            case "foreign", "за_рубежем", "за рубежем", "за кордоном", "другие страны" -> getForeign();
+            case "remote", "relocate", "релокейт", "удаленно", "віддалено" -> remoteAria;
             case "харків", "харьков", "kharkiv" -> kharkivAria;
             case "дніпро", "днепр", "dnipro" -> dniproAria;
             case "одеса", "одесса", "odesa" -> odesaAria;
@@ -100,7 +118,14 @@ public class ResumeUtil {
             case "wroclaw", "вроцлав" -> wroclawAria;
             case "gdansk", "гданськ", "гданск" -> gdanskAria;
             case "poznan", "познань" -> poznanAria;
-            default -> of(workplace);
+            default -> of(text);
         };
+    }
+
+    public static List<String> getForeign() {
+        List<String> foreign = new ArrayList<>(citiesWorld);
+        foreign.addAll(citiesPL);
+        foreign.addAll(foreignAria);
+        return foreign;
     }
 }
